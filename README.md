bc565873fd91d0b5dde9dd1a1f354d073d78b7a7
mc-b <marcel.bernet@ch-open.ch>
2020-12-22 11:38:29 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# lb02-thingspeak
-LB2 mit ThingSpeak
+# ThingSpeak
+## 1. Beschreibung
+ThingSpeak ist eine Open-Source Analyse-Plattform des Internet-of-Things-Anwendung (IoT). Mit einer API kann man live Datenströme in der Cloud visualisieren und analysieren. Die Daten von den jeweiligen Geräten können an ThingSpeak gesendet werden und es werden sofortige Visualisierungen und Analysen durchgeführt. Mit ThingSpeak können Ingenieure und Wissenschaftler IoT-Systeme prototypisieren und erstellen, ohne Server einrichten oder Web-Software entwickeln zu müssen.
+
+## 2. Installation
+1. ThinkSpeak Account erstellen
+2. ThinkSpeak Projekt erstellen
+3. ThinkSpeak Source Festlegen
+4. git klonen
+5. main.cpp anpassen (url, API-Key)
+6. kompilieren
+7. Deploy kompiliertes File auf das IOT-kit-v3
+8. IOT-Kit-v3 neu starten
+9. Installation fertig
+10. ThingSpeak Daten auslesen
+
+## 3. Dokumentation
+### 3.1 Daten übermitteln
+Die Daten werden über HTTPS an ThingSpeak übermittelt. Dadurch ist die Datenintegrität und Sicherheit gegeben. Die Daten werden via URL verschickt. 
+
+Die URL ist durch SSL verschlüsselt. Somit kann niemand ausser, ThinkSpeak die Daten lesen, da diese als SSL-Trust hinterlegt wurden.
+
+```
+https://api.thingspeak.com/update?[API-KEy]field1=[data],field2=[data]
+```
+
+### 3.2 Grafische Darstellung
+ThingSpeak verwendet für die grafische Darstellung zwei Werte. Einerseits sind es die gesendeten Daten und um dies in einem Lieniendiagramm darzustellen, nimmt es noch die Zeit, wann die Daten angekommen sind.
+
+### 3.3 Verarbeitung der Daten
+Mathlap ist die Firma, welche ThingSpeak anbietet. Mit diesem Tool könnte man die Daten noch weiter verarbeiten. Zum Beispiel könnte man den Mittelwert ausrechnen etc.
+
+### 3.4 Alerts
+Thingspeak kann als Alert zum Beispiel Twitter benutzen, oder ein Mail verschicken. Um Twitter aber zu benutzen, müsste man einen Twitter API-Key benutzen. Wir hatten aber alle keinen Twitter Account, deshalb haben wir uns auf unser Haubtproekt fokusiert, also unser Cloud-Dienst.
+
+## 4. Testprotokoll
+### 4.1 Kompillieren 
+| Testfall: 001     | Kompillieren                                                                                                         |
+| ----------------- | -------------------------------------------------------------------------------------------------------------------- |
+| Ziel:             | Das kompillierte File kann auf Thingspeak hochgeladen werden. Auf der seriellen Verbindung kommt es zu keinem Error. |
+| Beschreibung:     | Die Source von ./source kompillieren und auf das IOTkitV3 aufspielen. Danach das Board neu starten.                  |
+| Soll-Wert:        | Es kommen keine Errors auf der seriellen Verbinung                                                                   |
+| Ist-Wert:         | keine Errors                                                                                                         |
+| Analyse:          | funktioniert richtig.                                                                                                |
+| Weitere Schritte: | -                                                                                                                    |
+
+### 4.2 Display Anzeige
+| Testfall: 002     | Display Anzeige                                                        |
+| ----------------- | ---------------------------------------------------------------------- |
+| Ziel:             | Auf dem Display wird die Temperatur und Luftfäuchtigkeit angezeigt.    |
+| Beschreibung:     | Board an strom anschliessen und anschliessend in einen Sensor "huchen" |
+| Soll-Wert:        | Die Luftfäuchtigkeit steigt                                            |
+| Ist-Wert:         | Daten werden auf dem Display angezeigt                                 |
+| Analyse:          | Alles Richtig                                                          |
+| Weitere Schritte: | -                                                                      |
+
+### 4.3 Kommunikation ThingSpeak
+| Testfall: 003     | Kommunikation ThingSpeak                                                                       |
+| ----------------- | ---------------------------------------------------------------------------------------------- |
+| Ziel:             | Die Kommunikation erfolg verschlüsselt und die Daten werden grafisch auf ThingSpeak angezeigt. |
+| Beschreibung:     | Serielle Verbindung öffnen und schauen, ob die Daten über https verschickt werden.             |
+| Soll-Wert:        | Auf der seriellen Verbindung wird https ausgegeben.                                            |
+| Ist-Wert:         | https://update.thinkspeak.com                                                                  |
+| Analyse:          | Daten werden verschlüsselt übermittelt                                                         |
+| Weitere Schritte: | -                                                                                              |
---
+++ src/main.cpp
@@ -0,0 +1,137 @@
+/** LB02 - MultiTemp
+ */
+#include "mbed.h"
+#include "HTS221Sensor.h"
+#include "http_request.h"
+#include "network-helper.h"
+#include "OLEDDisplay.h"
+#include "DHT.h"
+#include "https_request.h"
+
+DHT sensor( A5, DHT11 );
+
+// UI
+OLEDDisplay oled( MBED_CONF_IOTKIT_OLED_RST, MBED_CONF_IOTKIT_OLED_SDA, MBED_CONF_IOTKIT_OLED_SCL );
+
+static DevI2C devI2c( MBED_CONF_IOTKIT_I2C_SDA, MBED_CONF_IOTKIT_I2C_SCL );
+static HTS221Sensor hum_temp(&devI2c);
+
+/** ThingSpeak URL und API Key ggf. anpassen */
+char host[] = "https://api.thingspeak.com/update";
+char key[] = "7XTWJM1BR3EUJ1PW";
+
+// I/O Buffer
+char message[1024];
+
+DigitalOut myled(D10);
+
+const char SSL_CA_PEM[] =  "-----BEGIN CERTIFICATE-----\n"
+"MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBs\n"
+"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
+"d3cuZGlnaWNlcnQuY29tMSswKQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5j\n"
+"ZSBFViBSb290IENBMB4XDTA2MTExMDAwMDAwMFoXDTMxMTExMDAwMDAwMFowbDEL\n"
+"MAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3\n"
+"LmRpZ2ljZXJ0LmNvbTErMCkGA1UEAxMiRGlnaUNlcnQgSGlnaCBBc3N1cmFuY2Ug\n"
+"RVYgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMbM5XPm\n"
+"+9S75S0tMqbf5YE/yc0lSbZxKsPVlDRnogocsF9ppkCxxLeyj9CYpKlBWTrT3JTW\n"
+"PNt0OKRKzE0lgvdKpVMSOO7zSW1xkX5jtqumX8OkhPhPYlG++MXs2ziS4wblCJEM\n"
+"xChBVfvLWokVfnHoNb9Ncgk9vjo4UFt3MRuNs8ckRZqnrG0AFFoEt7oT61EKmEFB\n"
+"Ik5lYYeBQVCmeVyJ3hlKV9Uu5l0cUyx+mM0aBhakaHPQNAQTXKFx01p8VdteZOE3\n"
+"hzBWBOURtCmAEvF5OYiiAhF8J2a3iLd48soKqDirCmTCv2ZdlYTBoSUeh10aUAsg\n"
+"EsxBu24LUTi4S8sCAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgGGMA8GA1UdEwEB/wQF\n"
+"MAMBAf8wHQYDVR0OBBYEFLE+w2kD+L9HAdSYJhoIAu9jZCvDMB8GA1UdIwQYMBaA\n"
+"FLE+w2kD+L9HAdSYJhoIAu9jZCvDMA0GCSqGSIb3DQEBBQUAA4IBAQAcGgaX3Nec\n"
+"nzyIZgYIVyHbIUf4KmeqvxgydkAQV8GK83rZEWWONfqe/EW1ntlMMUu4kehDLI6z\n"
+"eM7b41N5cdblIZQB2lWHmiRk9opmzN6cN82oNLFpmyPInngiK3BD41VHMWEZ71jF\n"
+"hS9OMPagMRYjyOfiZRYzy78aG6A9+MpeizGLYAiJLQwGXFK3xPkKmNEVX58Svnw2\n"
+"Yzi9RKR/5CYrCsSXaQ3pjOLAEFe4yHYSkVXySGnYvCoCWw9E1CAx2/S6cCZdkGCe\n"
+"vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep\n"
+"+OkuE6N36B9K\n"
+"-----END CERTIFICATE-----\n"
+"-----BEGIN CERTIFICATE-----\n"
+"MIIEsTCCA5mgAwIBAgIQBOHnpNxc8vNtwCtCuF0VnzANBgkqhkiG9w0BAQsFADBs\n"
+"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
+"d3cuZGlnaWNlcnQuY29tMSswKQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5j\n"
+"ZSBFViBSb290IENBMB4XDTEzMTAyMjEyMDAwMFoXDTI4MTAyMjEyMDAwMFowcDEL\n"
+"MAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3\n"
+"LmRpZ2ljZXJ0LmNvbTEvMC0GA1UEAxMmRGlnaUNlcnQgU0hBMiBIaWdoIEFzc3Vy\n"
+"YW5jZSBTZXJ2ZXIgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC2\n"
+"4C/CJAbIbQRf1+8KZAayfSImZRauQkCbztyfn3YHPsMwVYcZuU+UDlqUH1VWtMIC\n"
+"Kq/QmO4LQNfE0DtyyBSe75CxEamu0si4QzrZCwvV1ZX1QK/IHe1NnF9Xt4ZQaJn1\n"
+"itrSxwUfqJfJ3KSxgoQtxq2lnMcZgqaFD15EWCo3j/018QsIJzJa9buLnqS9UdAn\n"
+"4t07QjOjBSjEuyjMmqwrIw14xnvmXnG3Sj4I+4G3FhahnSMSTeXXkgisdaScus0X\n"
+"sh5ENWV/UyU50RwKmmMbGZJ0aAo3wsJSSMs5WqK24V3B3aAguCGikyZvFEohQcft\n"
+"bZvySC/zA/WiaJJTL17jAgMBAAGjggFJMIIBRTASBgNVHRMBAf8ECDAGAQH/AgEA\n"
+"MA4GA1UdDwEB/wQEAwIBhjAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIw\n"
+"NAYIKwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2Vy\n"
+"dC5jb20wSwYDVR0fBEQwQjBAoD6gPIY6aHR0cDovL2NybDQuZGlnaWNlcnQuY29t\n"
+"L0RpZ2lDZXJ0SGlnaEFzc3VyYW5jZUVWUm9vdENBLmNybDA9BgNVHSAENjA0MDIG\n"
+"BFUdIAAwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly93d3cuZGlnaWNlcnQuY29tL0NQ\n"
+"UzAdBgNVHQ4EFgQUUWj/kK8CB3U8zNllZGKiErhZcjswHwYDVR0jBBgwFoAUsT7D\n"
+"aQP4v0cB1JgmGggC72NkK8MwDQYJKoZIhvcNAQELBQADggEBABiKlYkD5m3fXPwd\n"
+"aOpKj4PWUS+Na0QWnqxj9dJubISZi6qBcYRb7TROsLd5kinMLYBq8I4g4Xmk/gNH\n"
+"E+r1hspZcX30BJZr01lYPf7TMSVcGDiEo+afgv2MW5gxTs14nhr9hctJqvIni5ly\n"
+"/D6q1UEL2tU2ob8cbkdJf17ZSHwD2f2LSaCYJkJA69aSEaRkCldUxPUd1gJea6zu\n"
+"xICaEnL6VpPX/78whQYwvwt/Tv9XBZ0k7YXDK/umdaisLRbvfXknsuvCnQsH6qqF\n"
+"0wGjIChBWUMo0oHjqvbsezt3tkBigAVBRQHvFwY+3sAzm2fTYS5yh+Rp/BIAV0Ae\n"
+"cPUeybQ=\n"
+"-----END CERTIFICATE-----";
+
+int main()
+{
+    uint8_t id;
+    float value1, value2, h = 0.0f, c = 0.0f;
+    int rc = 0;
+    
+    printf("\tLB02RedunantTemp\n");
+
+    /* Init all sensors with default params */
+    hum_temp.init(NULL);
+    hum_temp.enable();
+    
+    hum_temp.read_id(&id);
+    printf("HTS221  humidity & temperature    = 0x%X\r\n", id);    
+    
+    // Connect to the network with the default networking interface
+    // if you use WiFi: see mbed_app.json for the credentials
+    NetworkInterface* network = connect_to_default_network_interface();
+    
+    if (!network) {
+        printf("Cannot connect to the network, see serial output\n");
+        return 1;
+    }
+
+    while( 1 ) 
+    {
+        hum_temp.get_temperature(&value1);
+        hum_temp.get_humidity(&value2);
+        
+        rc = sensor.readData();
+        if ( rc == 0 )
+        {
+            c   = sensor.ReadTemperature(CELCIUS);
+            h   = sensor.ReadHumidity();
+        }
+        else
+            return  ( -1 );         // Fehler - Programm beenden
+    
+        
+        sprintf( message, "%s?key=%s&field1=%f&field2=%f&field3=%f&field4=%f", host, key, value1, value2, c, h );
+        printf( "%s\n", message );
+        oled.cursor( 1, 0 );
+        oled.printf( "temp: %3.2f   %4.2f\nhum : %3.2f   %4.2f", value1, c, value2, h );
+        
+        myled = 1;
+        HttpsRequest* get_req = new HttpsRequest( network, SSL_CA_PEM, HTTP_GET, message );
+
+        HttpResponse* get_res = get_req->send();
+        if (!get_res) 
+        {
+            printf("HttpsRequest failed (error code %d)\n", get_req->get_error());
+            return 1;
+        }
+        delete get_req;     
+        myled = 0;   
+        wait(10);
+    }
+}
